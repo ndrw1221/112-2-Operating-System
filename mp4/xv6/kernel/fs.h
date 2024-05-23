d38749d894673af695a9b33352eab71803dc1942
Andrew Hsieh <andrew891221@gmail.com>
2024-05-24 03:54:29 +0800

    Pass mp4_1 public

--- mp4/xv6/kernel/fs.h
+++ mp4/xv6/kernel/fs.h
@@ -1,9 +1,9 @@
 // On-disk file system format.
 // Both the kernel and user programs use this header file.
 
-
-#define ROOTINO  1   // root i-number
-#define BSIZE 1024  // block size
+#define ROOTINO 1                     // root i-number
+#define BSIZE 1024                    // block size
+#define NADDRS (BSIZE / sizeof(uint)) // number of addresses in a block, which is 256 in this case
 
 // Disk layout:
 // [ boot block | super block | log | inode blocks |
@@ -11,52 +11,56 @@
 //
 // mkfs computes the super block and builds an initial file system. The
 // super block describes the disk layout:
-struct superblock {
-  uint magic;        // Must be FSMAGIC
-  uint size;         // Size of file system image (blocks)
-  uint nblocks;      // Number of data blocks
-  uint ninodes;      // Number of inodes.
-  uint nlog;         // Number of log blocks
-  uint logstart;     // Block number of first log block
-  uint inodestart;   // Block number of first inode block
-  uint bmapstart;    // Block number of first free map block
+struct superblock
+{
+  uint magic;      // Must be FSMAGIC
+  uint size;       // Size of file system image (blocks)
+  uint nblocks;    // Number of data blocks
+  uint ninodes;    // Number of inodes.
+  uint nlog;       // Number of log blocks
+  uint logstart;   // Block number of first log block
+  uint inodestart; // Block number of first inode block
+  uint bmapstart;  // Block number of first free map block
 };
 
 #define FSMAGIC 0x10203040
 
 // TODO: bigfile
 // You may need to modify these.
-#define NDIRECT 12
-#define NINDIRECT (BSIZE / sizeof(uint))
-#define MAXFILE (NDIRECT + NINDIRECT)
+#define NDIRECT 6
+#define NINDIRECT (6 * NADDRS)                     // 6 * 256 = 1536
+#define NDINDIRECT (NADDRS * NADDRS)               // 256 * 256 = 65536
+#define MAXFILE (NDIRECT + NINDIRECT + NDINDIRECT) // 6 + 1536 + 65536 = 67078
 
 // On-disk inode structure
 
-struct dinode {
-  short type;           // File type
-  short major;          // Major device number (T_DEVICE only)
-  short minor;          // Minor device number (T_DEVICE only)
-  short nlink;          // Number of links to inode in file system
-  uint size;            // Size of file (bytes)
-  uint addrs[NDIRECT+1];   // Data block addresses
+struct dinode
+{
+  short type;                  // File type
+  short major;                 // Major device number (T_DEVICE only)
+  short minor;                 // Minor device number (T_DEVICE only)
+  short nlink;                 // Number of links to inode in file system
+  uint size;                   // Size of file (bytes)
+  uint addrs[NDIRECT + 6 + 1]; // Data block addresses
 };
 
 // Inodes per block.
-#define IPB           (BSIZE / sizeof(struct dinode))
+#define IPB (BSIZE / sizeof(struct dinode))
 
 // Block containing inode i
-#define IBLOCK(i, sb)     ((i) / IPB + sb.inodestart)
+#define IBLOCK(i, sb) ((i) / IPB + sb.inodestart)
 
 // Bitmap bits per block
-#define BPB           (BSIZE*8)
+#define BPB (BSIZE * 8)
 
 // Block of free map containing bit for block b
-#define BBLOCK(b, sb) ((b)/BPB + sb.bmapstart)
+#define BBLOCK(b, sb) ((b) / BPB + sb.bmapstart)
 
 // Directory is a file containing a sequence of dirent structures.
 #define DIRSIZ 14
 
-struct dirent {
+struct dirent
+{
   ushort inum;
   char name[DIRSIZ];
 };
